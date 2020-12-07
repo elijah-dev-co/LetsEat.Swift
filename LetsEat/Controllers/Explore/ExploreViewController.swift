8184e3ec06264dc2b2d4fe2c0275880f1bfca4bc
elijah-dev-co <72037832+elijah-dev-co@users.noreply.github.com>
2020-12-07 00:24:50 -0600

    LetsEat.swift

---
+++ LetsEat/Controllers/Explore/ExploreViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  CollectionViewTest
+//
+//  Created by Craig Clayton on 6/30/17.
+//  Copyright © 2017 Cocoa Academy. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let manager = ExploreDataManager()
+    var selectedCity:LocationItem?
+    var headerView: ExploreHeaderView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        initialize()
+    }
+}
+
+
+
+// MARK: Private Extension
+private extension ExploreViewController {
+    
+    func initialize() {
+        manager.fetch()
+    }
+   
+    @IBAction func unwindLocationCancel(segue:UIStoryboardSegue) {}
+    
+    @IBAction func unwindLocationDone(segue:UIStoryboardSegue) {
+        if let viewController = segue.source as? LocationViewController {
+            selectedCity = viewController.selectedCity
+            if let location = selectedCity {
+                headerView.lblLocation.text = location.full
+            }
+        }
+    }
+}
+// MARK: UICollectionViewDataSource
+extension ExploreViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath)
+        headerView = header as? ExploreHeaderView
+        return headerView
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "exploreCell", for: indexPath) as! ExploreCell
+        
+        let item = manager.explore(at: indexPath)
+        cell.lblName.text = item.name
+        cell.imgExplore.image = UIImage(named: item.image)
+        
+        return cell
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return manager.numberOfItems()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ LetsEat/Controllers/Explore/View/ExploreHeaderView.swift
@@ -0,0 +1,12 @@
+//
+//  ExploreHeaderView.swift
+//  LetsEat
+//
+//  Created by Zhimin Gao on 3/9/20.
+//  Copyright © 2020 Cocoa Academy. All rights reserved.
+//
+
+import UIKit
+class ExploreHeaderView: UICollectionReusableView {
+    @IBOutlet weak var lblLocation:UILabel!
+}
